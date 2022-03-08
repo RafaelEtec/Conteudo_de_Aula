3f02c94779b5fd24eaabe97663d9651b63c32931
Rafael.fgoulart1 <Rafael.fgoulart1@CAS560797W1019.senacsp.edu.br>
2022-03-07 22:21:18 -0300

    API 07/03/2022

--- src/main/java/API_07_03_2022/Funcoes.java
+++ src/main/java/API_07_03_2022/Funcoes.java
@@ -1,7 +1,23 @@
 package API_07_03_2022;
+import java.util.Scanner;
 /**
    por Rafael Ferreira Goulart
 **/
 public class Funcoes {
+    public static void main(String[] args) {
+        Scanner ent = new Scanner(System.in);
+        
+        System.out.println("Prim. Valor: ");
+        double n1 = ent.nextDouble();
+        System.out.println("Seg. Valor: ");
+        double n2 = ent.nextDouble();
+        System.out.println("Terc. Valor: ");
+        double n3 = ent.nextDouble();
+        
+    }
     
+    public double CalcularMedia(double nota1, double nota2, double nota3) {
+        double media = (nota1 + nota2 + nota3) / 3;
+        return media;
+    }
 }
