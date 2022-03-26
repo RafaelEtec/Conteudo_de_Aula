0380f6e4f1ce7221c4eb054453d992c0df19ab34
rafael.fgoulart1 <rafael.fgoulart1@CAS511911C138.senacsp.edu.br>
2022-03-25 21:38:41 -0300

    Finalizado

---
+++ src/main/java/API_25_03_2022/PI_ADO6.java
@@ -0,0 +1,47 @@
+package API_25_03_2022;
+import java.util.Scanner;
+/** 
+   por Rafael Ferreira Goulart
+**/
+public class PI_ADO6 {
+    public static void main(String[] args) {
+        Scanner ent = new Scanner(System.in);
+        String resp;
+        boolean correto = false;
+        int tentativas = 3;
+        System.out.println(
+                  "Segundo o Sistema de Numeração Egípcio.\n"
+                + "O número 1000 corresponde a: \n"
+                + "(A) Bastão\n"
+                + "(B) Rolo de Corda\n"
+                + "(C) Peixe\n"
+                + "(D) Flor de Lótus\n"
+                + "(E) Homem\n");
+        
+        do {
+            resp = ent.next();
+            switch (resp.toLowerCase()) {
+                case "a":
+                    System.out.println("Resposta Incorreta!");
+                    tentativas--;
+                    break;
+                case "b":
+                    System.out.println("Resposta Incorreta!");
+                    tentativas--;
+                    break;
+                case "c":
+                    System.out.println("Resposta Incorreta!");
+                    tentativas--;
+                    break;
+                case "d":
+                    System.out.println("<d> É a Resposta Correta!");
+                    correto = true;
+                    break;
+                case "e":
+                    System.out.println("Resposta Incorreta!");
+                    tentativas--;
+                    break;
+            }
+        } while (!correto && tentativas != 0);
+    }
+}
