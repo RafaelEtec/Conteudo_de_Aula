70b9198e2e5dd4d928abe6fa809238c6abe0157c
Rafael.fgoulart1 <Rafael.fgoulart1@CAS468300W1012.senacsp.edu.br>
2022-04-01 20:44:26 -0300

    Andamento da ADO7

--- src/main/java/API_01_04_2022/PI_ADO7.java
+++ src/main/java/API_01_04_2022/PI_ADO7.java
@@ -1,26 +1,35 @@
 package API_01_04_2022;
+import java.util.Arrays;
 import java.util.Scanner;
+import java.util.List;
+import java.util.Collections;
 /** 
    por Rafael Ferreira Goulart
 **/
 public class PI_ADO7 {
     public static void main(String[] args) {
         Scanner ent = new Scanner(System.in);
+        
         String resp;
         boolean correto = false;
         int tentativas = 3;
-        String pergunta =
+        String pergunta = 
                   "Segundo o Sistema de Numeração Egípcio.\n"
-                + "O número 1000 corresponde a: \n"
-                + "(A) Bastão\n"
-                + "(B) Rolo de Corda\n"
-                + "(C) Peixe\n"
-                + "(D) Flor de Lótus\n"
-                + "(E) Homem\n";
+                + "O número 1000 corresponde a: \n";
+        String[] alternativas = {
+                  "(A) Bastão"
+                , "(B) Rolo de Corda"
+                , "(C) Peixe"
+                , "(D) Flor de Lótus"
+                , "(E) Homem"};
         
         do {
             System.out.println(pergunta);
+            for (int pos = 0; pos < alternativas.length; pos++) {
+                System.out.println(alternativas[pos]);
+            }
             resp = ent.next();
+            
             switch (resp.toLowerCase()) {
                 case "a":
                     System.out.println("Resposta Incorreta!");
@@ -43,9 +52,20 @@ public class PI_ADO7 {
                     tentativas--;
                     break;
             }
+            
+            trocarAlternativas(alternativas);
         } while (!correto && tentativas != 0);
         if (tentativas == 0) {
             System.out.println("Você excedeu o número de tentativas!");
         }
+    }
+    
+    public static String[] trocarAlternativas(String[] alternativas) {
+        
+        List<String> strAlternativas = Arrays.asList(alternativas);
+        Collections.shuffle(strAlternativas);
+        strAlternativas.toArray(alternativas);
+        
+        return alternativas;
     }
 }
