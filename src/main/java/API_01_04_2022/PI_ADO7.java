b3b01ab4a501df86d4a4c657ffe223b200322d40
Rafael.fgoulart1 <Rafael.fgoulart1@CAS468300W1012.senacsp.edu.br>
2022-04-01 19:43:43 -0300

    commit na branch backup

---
+++ src/main/java/API_01_04_2022/PI_ADO7.java
@@ -0,0 +1,51 @@
+package API_01_04_2022;
+import java.util.Scanner;
+/** 
+   por Rafael Ferreira Goulart
+**/
+public class PI_ADO7 {
+    public static void main(String[] args) {
+        Scanner ent = new Scanner(System.in);
+        String resp;
+        boolean correto = false;
+        int tentativas = 3;
+        String pergunta =
+                  "Segundo o Sistema de Numeração Egípcio.\n"
+                + "O número 1000 corresponde a: \n"
+                + "(A) Bastão\n"
+                + "(B) Rolo de Corda\n"
+                + "(C) Peixe\n"
+                + "(D) Flor de Lótus\n"
+                + "(E) Homem\n";
+        
+        do {
+            System.out.println(pergunta);
+            resp = ent.next();
+            switch (resp.toLowerCase()) {
+                case "a":
+                    System.out.println("Resposta Incorreta!");
+                    tentativas--;
+                    break;
+                case "b":
+                    System.out.println("Resposta Incorreta!");
+                    tentativas--;
+                    break;
+                case "c":
+                    System.out.println("Resposta Incorreta!");
+                    tentativas--;
+                    break;
+                case "d":
+                    System.out.println("<d> É a Resposta Correta!");
+                    correto = true;
+                    break;
+                case "e":
+                    System.out.println("Resposta Incorreta!");
+                    tentativas--;
+                    break;
+            }
+        } while (!correto && tentativas != 0);
+        if (tentativas == 0) {
+            System.out.println("Você excedeu o número de tentativas!");
+        }
+    }
+}
